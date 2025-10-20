b61f0d9738d3b2bf0ab93ea2a980c67812756993
JanaNafei <jananafei103@gmail.com>
2025-10-20 10:36:42 +0300

    Update Product.java

--- Lab4/src/main/java/com/mycompany/lab4/Product.java
+++ Lab4/src/main/java/com/mycompany/lab4/Product.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.lab4;
-import java.util.*;
-/**
- *
- * @author orignal store
- */
+
 public class Product {
 private final String productID;
 private String productName;
